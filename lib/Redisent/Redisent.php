9599b0305fd9ddd01f96a78294b34f7538183660
Lee Leathers <leeleathers@gmail.com>
2012-07-23 02:45:12 -0700

    updated Redisent.php interface class to support redis 2.2 and prior

--- lib/Redisent/Redisent.php
+++ lib/Redisent/Redisent.php
@@ -2,6 +2,7 @@
 /**
  * Redisent, a Redis interface for the modest
  * @author Justin Poliey <jdp34@njit.edu>
+ * @author Lee Leathers <leeleathers@gmail.com>
  * @copyright 2009 Justin Poliey <jdp34@njit.edu>
  * @license http://www.opensource.org/licenses/mit-license.php The MIT License
  * @package Redisent
@@ -28,105 +29,142 @@ class Redisent {
     private $__sock;
 
     /**
-     * Host of the Redis server
-     * @var string
-     * @access public
+     * Flag indicating whether or not commands are being pipelined
+     * @var boolean
+     * @access private
      */
-    public $host;
+    private $pipelined = FALSE;
 
     /**
-     * Port on which the Redis server is running
-     * @var integer
-     * @access public
+     * The queue of commands to be sent to the Redis server
+     * @var array
+     * @access private
      */
-    public $port;
+    private $queue = array();
 
     /**
      * Creates a Redisent connection to the Redis server on host {@link $host} and port {@link $port}.
      * @param string $host The hostname of the Redis server
      * @param integer $port The port number of the Redis server
      */
-    function __construct($host, $port = 6379) {
-        $this->host = $host;
-        $this->port = $port;
-				$this->establishConnection();
-    }
-
-    function establishConnection() {
-        $this->__sock = fsockopen($this->host, $this->port, $errno, $errstr);
+    public function __construct($host, $port = 6379) {
+        $this->__sock = @fsockopen($host, $port);
         if (!$this->__sock) {
-            throw new Exception("{$errno} - {$errstr}");
+            throw new Exception("Cannot open socket to $host:$port");
         }
     }
 
-    function __destruct() {
+    public function __destruct() {
         fclose($this->__sock);
     }
-
-    function __call($name, $args) {
-
-        /* Build the Redis unified protocol command */
-        array_unshift($args, strtoupper($name));
-        $command = sprintf('*%d%s%s%s', count($args), CRLF, implode(array_map(array($this, 'formatArgument'), $args), CRLF), CRLF);
-
-        /* Open a Redis connection and execute the command */
-        for ($written = 0; $written < strlen($command); $written += $fwrite) {
-            $fwrite = fwrite($this->__sock, substr($command, $written));
-            if ($fwrite === FALSE) {
-                throw new Exception('Failed to write entire command to stream');
+  
+    /**
+     * Returns the Redisent instance ready for pipelining.
+     * Redis commands can now be chained, and the array of the responses will be returned when {@link uncork} is called.
+     * @see uncork
+     * @access public
+     */
+    public function pipeline() {
+        $this->pipelined = TRUE;
+        return $this;
+    }
+  
+    /**
+     * Flushes the commands in the pipeline queue to Redis and returns the responses.
+     * @see pipeline
+     * @access public
+     */
+    public function uncork() {
+        /* Open a Redis connection and execute the queued commands */
+        foreach ($this->queue as $command) {
+            for ($written = 0; $written < strlen($command); $written += $fwrite) {
+                $fwrite = fwrite($this->__sock, substr($command, $written));
+                if ($fwrite === FALSE) {
+                    throw new Exception('Failed to write entire command to stream');
+                }
             }
         }
 
+        // Read in the results from the pipelined commands
+        $responses = array();
+        for ($i = 0; $i < count($this->queue); $i++) {
+            $responses[] = $this->readResponse();
+        }
+
+        // Clear the queue and return the response
+        $this->queue = array();
+        if ($this->pipelined) {
+            $this->pipelined = FALSE;
+            return $responses;
+        } else {
+            return $responses[0];
+        }
+    }
+
+    public function __call($name, $args) {
+        /* Build the Redis unified protocol command */
+        array_unshift($args, strtoupper($name));
+        $command = sprintf('*%d%s%s%s', count($args), CRLF, implode(array_map(function($arg) {
+            return sprintf('$%d%s%s', strlen($arg), CRLF, $arg);
+        }, $args), CRLF), CRLF);
+
+        /* Add it to the pipeline queue */
+        $this->queue[] = $command;
+
+        if ($this->pipelined) {
+            return $this;
+        } else {
+            return $this->uncork();
+        }
+
+    }
+  
+    private function readResponse() {
         /* Parse the response based on the reply identifier */
         $reply = trim(fgets($this->__sock, 512));
         switch (substr($reply, 0, 1)) {
             /* Error reply */
             case '-':
-                throw new RedisException(substr(trim($reply), 4));
+                throw new RedisException(trim(substr($reply, 4)));
                 break;
             /* Inline reply */
             case '+':
                 $response = substr(trim($reply), 1);
+                if ($response === 'OK') {
+                    $response = TRUE;
+                }
                 break;
             /* Bulk reply */
             case '$':
-                $response = null;
+                $response = NULL;
                 if ($reply == '$-1') {
                     break;
                 }
                 $read = 0;
-                $size = substr($reply, 1);
-                do {
-                    $block_size = ($size - $read) > 1024 ? 1024 : ($size - $read);
-                    $response .= fread($this->__sock, $block_size);
-                    $read += $block_size;
-                } while ($read < $size);
+                $size = intval(substr($reply, 1));
+                if ($size > 0) {
+                    do {
+                        $block_size = ($size - $read) > 1024 ? 1024 : ($size - $read);
+                        $r = fread($this->__sock, $block_size);
+                        if ($r === FALSE) {
+                            throw new \Exception('Failed to read response from stream');
+                        } else {
+                            $read += strlen($r);
+                            $response .= $r;
+                        }
+                    } while ($read < $size);
+                }
                 fread($this->__sock, 2); /* discard crlf */
                 break;
             /* Multi-bulk reply */
             case '*':
-                $count = substr($reply, 1);
+                $count = intval(substr($reply, 1));
                 if ($count == '-1') {
-                    return null;
+                    return NULL;
                 }
                 $response = array();
                 for ($i = 0; $i < $count; $i++) {
-                    $bulk_head = trim(fgets($this->__sock, 512));
-                    $size = substr($bulk_head, 1);
-                    if ($size == '-1') {
-                        $response[] = null;
-                    }
-                    else {
-                        $read = 0;
-                        $block = "";
-                        do {
-                            $block_size = ($size - $read) > 1024 ? 1024 : ($size - $read);
-                            $block .= fread($this->__sock, $block_size);
-                            $read += $block_size;
-                        } while ($read < $size);
-                        fread($this->__sock, 2); /* discard crlf */
-                        $response[] = $block;
-                    }
+                    $response[] = $this->readResponse();
                 }
                 break;
             /* Integer reply */
@@ -134,14 +172,11 @@ class Redisent {
                 $response = intval(substr(trim($reply), 1));
                 break;
             default:
-                throw new RedisException("invalid server response: {$reply}");
+                throw new RedisException("Unknown response: {$reply}");
                 break;
         }
         /* Party on */
         return $response;
     }
 
-    private function formatArgument($arg) {
-        return sprintf('$%d%s%s', strlen($arg), CRLF, $arg);
-    }
 }
